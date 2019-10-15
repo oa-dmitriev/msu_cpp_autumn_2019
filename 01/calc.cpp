124273f528d742492dd4498f6f88c1a0ff908b49
Oleg Dmitriev <dmitriev-oleg@list.ru>
2019-10-15 03:13:38 +0300

    fixed

--- 01/calc.cpp
+++ 01/calc.cpp
@@ -2,19 +2,33 @@
 #include <vector>
 #include <algorithm>
 #include <exception>
+#include <cstring>
 
 class WrongInput : public std::exception {
-	public:
-		const char* what() const noexcept {
-			return "Wrong input format: ";
-		}
+public:
+	WrongInput(const std::string &msg, int line, int pos)
+		: msg(msg), line(line), pos(pos) {}
+	const char* what() const noexcept {
+		return msg.c_str();
+	}
+	
+	int getErrorLine() const throw() {
+		return line;
+	}
+	int getPos() const throw() {
+		return pos;
+	}
+private:
+	std::string msg;
+	int line;
+	int pos;
 };
 
-bool is_number(const std::string& s) {
+inline bool is_number(const std::string& s) {
 	return std::all_of(s.begin(), s.end(), isdigit);
 }
 
-double next_number(std::vector<std::string> &v, size_t &pos) {
+double next_number(const std::vector<std::string> &v, size_t &pos) {
 	int sign = 1;
 	double num;
 	if (v[pos] == "-") {
@@ -24,12 +38,12 @@ double next_number(std::vector<std::string> &v, size_t &pos) {
 	if (pos < v.size() && is_number(v[pos])) {
 		return sign * atof(v[pos].c_str());
 	}
-	throw WrongInput();
+	throw WrongInput("Expected number but got operator", __LINE__, pos);
 }
 
-double mul(std::vector<std::string> &v, size_t &pos) {
+double mul(const std::vector<std::string> &v, size_t &pos) {
 	if (pos >= v.size()) {
-		throw WrongInput();
+		throw WrongInput("Expected number but got nothing", __LINE__, pos);
 	}
 
 	double res = next_number(v, pos);
@@ -46,44 +60,42 @@ double mul(std::vector<std::string> &v, size_t &pos) {
 	return res;
 }
 
-double sum(std::vector<std::string> &v, size_t &pos) {
+double sum(const std::vector<std::string> &v, size_t &pos) {
 	double res = mul(v, pos);
 	while (pos < v.size()) {
 		if (v[pos] == "+") {
 			res += mul(v, ++pos);
 		} else 	if (v[pos] == "-") {
 			res -= mul(v, ++pos);
+		} else {
+			throw WrongInput("Wrong symbol", __LINE__, pos);
 		}
 	}
 	return res;
-}
-
-bool is_op(char c) {
-	return c == '+' || c == '-' || c == '*' || c == '/';
 }
 
 std::vector<std::string> parse(std::string &s) {
 	s.erase(remove_if(s.begin(), s.end(), isspace), s.end());
 	std::vector<std::string> v;
 	for (int i = 0; i < s.size(); ++i) {
-		std::string token;
-		if (isdigit(s[i])) {
-			while(isdigit(s[i])) {
-				token.append(1, s[i]);
-				i++;
-			}
-			i--;
-		} else if (is_op(s[i])) {
+		std::string token = "";
+		if (!isdigit(s[i])) {
 			token.append(1, s[i]);
-		} else {
-			throw WrongInput();
+			v.push_back(token);
+			continue;
 		}
+
+		while (isdigit(s[i])) {
+			token.append(1, s[i]);
+			i++;
+		}
+		i--;
 		v.push_back(token);
 	}
 	return v;
 }
 
-void print(std::vector<std::string> &v) {
+void print(const std::vector<std::string> &v) {
 	for (int i = 0; i < v.size(); ++i) {
 		std::cout << v[i] << " ";
 	}
@@ -93,19 +105,22 @@ void print(std::vector<std::string> &v) {
 int main(int argc, char* argv[]) {
 	std::string expr;
 	if (argc != 2) {
-		std::cerr << "Wrong input format: " << expr << std::endl;
+		std::cerr << "plese enter a string as an argument" << expr << std::endl;
 		return 1;
 	}
 	expr = std::string(argv[1]);
 	std::vector<std::string> v;
+	v = parse(expr);
 	try {
-		v = parse(expr);
 		double result;
 		size_t pos = 0;
 		result = sum(v, pos);
 		std::cout << result;
 	} catch (WrongInput& ex) {
-		std::cerr << ex.what() << expr << std::endl;
+		std::cerr << "Error in line: " << ex.getErrorLine()
+			  << ". " << ex.what() << " at position "
+			  << ex.getPos() << " of the input expression: " 
+			  << expr << std::endl;
 		return 1;
 	}
 	return 0;
