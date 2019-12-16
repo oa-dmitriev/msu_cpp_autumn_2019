b4dc804e772c688ff96513adfea1bf0718ee348a
Oleg Dmitriev <dmitriev-oleg@list.ru>
2019-12-16 14:29:05 +0300

    added atomic flag

--- 08/pool.cpp
+++ 08/pool.cpp
@@ -10,65 +10,65 @@
 
 class ThreadPool {
 public:
-	using Task = std::function<void ()>;
-	using Lock = std::unique_lock<std::mutex>;
+    using Task = std::function<void ()>;
+    using Lock = std::unique_lock<std::mutex>;
 
-	explicit ThreadPool(size_t poolSize) 
-			: poolSize_(poolSize), mStop_(false), count_(0) {
-		for (auto i = 0u; i < poolSize_; ++i) {
-			auto thread = [=]{
-				while (true) {
-					Task task;
-					{
-						Lock lock(m_);
-						cv_.wait(lock, [=] { 
+    explicit ThreadPool(size_t poolSize) 
+            : poolSize_(poolSize), mStop_(false), count_(0) {
+        for (auto i = 0u; i < poolSize_; ++i) {
+            auto thread = [=]{
+                while (true) {
+                    Task task;
+                    {
+                        Lock lock(m_);
+                        cv_.wait(lock, [=] { 
                             return mStop_ || !mTasks_.empty();
                         });
-						if (mStop_ && mTasks_.empty() || count_ > poolSize_) {
-							break;
-						}
-						task = std::move(mTasks_.front());
-						mTasks_.pop();
-					}
-					task();
-				}
-			};
-			mThreads_.emplace_back(thread);	
-		}
-	}
-	
-	~ThreadPool() {
-		mStop_ = true;
-		cv_.notify_all();	
-		for (auto &thread : mThreads_) {
-			thread.join();
-		}
-	}
-	
-	template <class Func, class... Args>
-	auto exec(Func func, Args... args) -> 
-			std::future<decltype(func(args...))> {
-		using returnType = decltype(func(args...));
-		auto wrapper = std::make_shared<std::packaged_task<
-				returnType (Args...)>>(std::move(func));
-		{
-			Lock lock(m_);
-			mTasks_.emplace([=] { (*wrapper)(args...); });
-		}
-		cv_.notify_one();
+                        if (mStop_ && mTasks_.empty() || count_ > poolSize_) {
+                            break;
+                        }
+                        task = std::move(mTasks_.front());
+                        mTasks_.pop();
+                    }
+                    task();
+                }
+            };
+            mThreads_.emplace_back(thread);	
+        }
+    }
+    
+    ~ThreadPool() {
+        mStop_ = true;
+        cv_.notify_all();	
+        for (auto &thread : mThreads_) {
+            thread.join();
+        }
+    }
+    
+    template <class Func, class... Args>
+    auto exec(Func func, Args... args) -> 
+            std::future<decltype(func(args...))> {
+        using returnType = decltype(func(args...));
+        auto wrapper = std::make_shared<std::packaged_task<
+                returnType (Args...)>>(std::move(func));
+        {
+            Lock lock(m_);
+            mTasks_.emplace([=] { (*wrapper)(args...); });
+        }
+        cv_.notify_one();
         ++count_;
-		return wrapper->get_future();	
-	}
-	
+        return wrapper->get_future();	
+    }
+    
 private:
-	std::mutex m_;
-	std::condition_variable cv_;
-	std::atomic<bool> mStop_;
+    std::mutex m_;
+    std::condition_variable cv_;
+    std::atomic<bool> mStop_;
     std::atomic<size_t> count_;
-	std::vector<std::thread> mThreads_;
-	std::queue<Task> mTasks_;
-	size_t poolSize_;
-	
+    std::vector<std::thread> mThreads_;
+    std::queue<Task> mTasks_;
+    size_t poolSize_;
+    
 };
 
 struct A {};
@@ -76,11 +76,11 @@ struct A {};
 void foo(const A&) {};
 
 int main() {
-	ThreadPool pool(8);
-	auto task1 = pool.exec(foo, A());
-	task1.get();
+    ThreadPool pool(8);
+    auto task1 = pool.exec(foo, A());
+    task1.get();
 
-	auto task2 = pool.exec([]() { return 1; });
-	task2.get();
-	return 0;
+    auto task2 = pool.exec([]() { return 1; });
+    task2.get();
+    return 0;
 }
